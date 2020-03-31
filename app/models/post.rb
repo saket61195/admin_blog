854f96028de1f0a78b909789d5c19a65a91d435f
saket61195 <saket61195@gmail.com>
2020-03-31 10:44:11 +0530

    show post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :moderator
   has_many :comments
   has_many :tags, through: :post_tags
+  def self.matching_title_or_content search
+  	where("title Like? or content Like?","%#{search}%","%#{search}%")
+  end
 end
